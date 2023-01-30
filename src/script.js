b3579d823c32c70a32969535b3ec657ab863f7ca
hachaturoff <oleg.hachaturoff@mail.ru>
2023-01-30 13:22:20 +0300

    I made axios run in the browser

--- src/script.js
+++ src/script.js
@@ -1,25 +1,33 @@
 
 // Написан на NodeJS с использованием модулей axios и jsdom
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+// const axios = require('axios/dist/browser/axios.cjs');
 
-const axios = require('axios'); // Подключение модуля axios для скачивания страницы
-const fs = require('fs'); // Подключение встроенного в NodeJS модуля fs для работы с файловой системой
-const jsdom = require("jsdom"); // Подключение модуля jsdom для работы с DOM-деревом (1)
-const { JSDOM } = jsdom; // Подключение модуля jsdom для работы с DOM-деревом (2)
+// import jsdom from 'jsdom';
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
 
+let text = document.getElementById('text')
+let btn = document.getElementById('btn')
 
 
 function parccurent() {
+
     axios.get("https://myfin.by/currency/minsk")
+
         .then(response => {
             let currentPage = response.data; // Запись полученного результата - сайт
-            const dom = new JSDOM(currentPage);
-            let current = dom.window.document.querySelector('#st-container > div > div > div > div > div > div > div > div > div.mt-10 > div > div > div.c-main > div:nth-child(1) > div.c-section.bg-pearl-white > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)').textContent
-            console.log(current)
+            // const dom = new JSDOM(currentPage);
+            // let current = dom.window.document.querySelector('#st-container > div > div > div > div > div > div > div > div > div.mt-10 > div > div > div.c-main > div:nth-child(1) > div.c-section.bg-pearl-white > div > div > table > tbody > tr:nth-child(1) > td:nth-child(2)').textContent
+            // console.log(current)
 
-            fs.appendFileSync('articles.text',current);
+            // fs.appendFileSync('articles.text',current);
         })
 }
 
-// setInterval(parccurent, 1000)
-
 parccurent()
+
+btn.addEventListener('click', () => {
+    console.log('hi')
+})
